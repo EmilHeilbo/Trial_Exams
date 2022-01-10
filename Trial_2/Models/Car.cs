93a8336f2f494ba60a779488cb4fd663a9377de0
EmilHeilbo <19676610+EmilHeilbo@users.noreply.github.com>
2022-01-10 08:11:09 +0100

    Formatting corrected to conform with C# conventions

--- Trial_2/Models/Car.cs
+++ Trial_2/Models/Car.cs
@@ -2,19 +2,19 @@ namespace Models;
 
 public class Car
 {
-  public string Name { get; }
-  public string Cylinders { get; }
-  public string Country { get; }
-  
-  public Car(string name, string cylinders, string country)
-  {
-    Name = name;
-    Cylinders = cylinders;
-    Country = country;
-  }
+    public string Name { get; }
+    public string Cylinders { get; }
+    public string Country { get; }
 
-  public override string ToString()
-  {
-    return $"name: \"{Name}\";\ncylinders: : \"{Cylinders}\";\ncountry: : \"{Country}\";";
-  }
+    public Car(string name, string cylinders, string country)
+    {
+        Name = name;
+        Cylinders = cylinders;
+        Country = country;
+    }
+
+    public override string ToString()
+    {
+        return $"name: \"{Name}\";\ncylinders: : \"{Cylinders}\";\ncountry: : \"{Country}\";";
+    }
 }
--- Trial_2/Program.cs
+++ Trial_2/Program.cs
@@ -6,67 +6,67 @@ var _cars = new ArrayList();
 
 void Read()
 {
-  var settings = new XmlReaderSettings();
-  settings.IgnoreComments = true;
-  settings.IgnoreProcessingInstructions = true;
-  settings.IgnoreWhitespace = true;
-  settings.Async = true;
-  var reader = XmlReader.Create("cars.xml", settings);
-  reader.MoveToContent();
-  string name = "",
-      cylinders = "",
-      country = "";
-  while (reader.Read())
-  {
-    if (reader.IsStartElement())
+    var settings = new XmlReaderSettings();
+    settings.IgnoreComments = true;
+    settings.IgnoreProcessingInstructions = true;
+    settings.IgnoreWhitespace = true;
+    settings.Async = true;
+    var reader = XmlReader.Create("cars.xml", settings);
+    reader.MoveToContent();
+    string name = "",
+        cylinders = "",
+        country = "";
+    while (reader.Read())
     {
-      if (reader.HasAttributes && reader.Name == "car")
-      {
-        // Find attribute "name" in node <car>
-        for (int i = 0; i < reader.AttributeCount; i++)
+        if (reader.IsStartElement())
         {
-          reader.MoveToAttribute(i);
-          if (reader.Name == "name")
-            name = reader.GetAttribute(i);
+            if (reader.HasAttributes && reader.Name == "car")
+            {
+                // Find attribute "name" in node <car>
+                for (int i = 0; i < reader.AttributeCount; i++)
+                {
+                    reader.MoveToAttribute(i);
+                    if (reader.Name == "name")
+                        name = reader.GetAttribute(i);
+                }
+            }
+            else
+            {
+                switch (reader.Name)
+                {
+                    case "cylinders": cylinders = reader.ReadString(); break;
+                    case "country": country = reader.ReadString(); break;
+                }
+                // Console.WriteLine("{0}: {1};", reader.Name, reader.ReadString());
+            }
         }
-      }
-      else
-      {
-        switch (reader.Name)
-        {
-          case "cylinders": cylinders = reader.ReadString(); break;
-          case "country": country = reader.ReadString(); break;
-        }
-        // Console.WriteLine("{0}: {1};", reader.Name, reader.ReadString());
-      }
+        else if (reader.Name == "car" && !reader.IsStartElement())
+            _cars.Add(new Car(name, cylinders, country));
     }
-    else if (reader.Name == "car" && !reader.IsStartElement())
-      _cars.Add(new Car(name, cylinders, country));
-  }
-  reader.Close();
-  foreach (var car in _cars)
-    Console.WriteLine($"{car.ToString()}\n");
+    reader.Close();
+    foreach (var car in _cars)
+        Console.WriteLine($"{car.ToString()}\n");
 }
 
 void Write()
 {
-  var settings = new XmlWriterSettings();
-  settings.Indent = true;
-  settings.NewLineChars = "\r\n";
-  var writer = XmlWriter.Create("out.xml", settings);
-  writer.WriteStartDocument();
-  writer.WriteStartElement("cars");
-  foreach (Car car in _cars)
-  {
-    writer.WriteStartElement("car");
-    writer.WriteAttributeString("name", car.Name);
-    writer.WriteElementString("cylinders", car.Cylinders);
-    writer.WriteElementString("country", car.Country);
+    var settings = new XmlWriterSettings();
+    settings.Indent = true;
+    settings.NewLineChars = "\r\n";
+    var writer = XmlWriter.Create("out.xml", settings);
+    writer.WriteStartDocument();
+    writer.WriteStartElement("cars");
+    foreach (Car car in _cars)
+    {
+        writer.WriteStartElement("car");
+        writer.WriteAttributeString("name", car.Name);
+        writer.WriteElementString("cylinders", car.Cylinders);
+        writer.WriteElementString("country", car.Country);
+        writer.WriteEndElement();
+    }
     writer.WriteEndElement();
-  }
-  writer.WriteEndElement();
-  writer.Flush();
-  writer.Close();
+    writer.Flush();
+    writer.Close();
 }
 
 Read();

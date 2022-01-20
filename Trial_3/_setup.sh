98ae5df23ba3ebe2dd58e453ec9e249caa11f104
EmilHeilbo <19676610+EmilHeilbo@users.noreply.github.com>
2022-01-20 22:25:36 +0100

    Migrations, updated View, shell script

---
+++ Trial_3/_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export PATH="$PATH:/home/ethan/.dotnet/tools"
+
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+# Changed files (See commit #9485fd4)
+dotnet ef migrations add Product
+dotnet ef database update
